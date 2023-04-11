1d359a9c8d2064f071329009b978a6a15317cee6
distepstar <jobsdzihinss@gmail.com>
2023-04-11 12:33:07 -0400

    add note

--- scrape_src/selenium_driver/__init__.py
+++ scrape_src/selenium_driver/__init__.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_chrome_driver(headless=True):
+    # define webdrive in chrome
     options = webdriver.ChromeOptions()
     options.headless = headless
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
